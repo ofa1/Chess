84d83452af6823c919ec44dd595d1f27999687cc
Omair Ahmed <omair.ahmed@gmail.com>
2014-05-01 11:02:53 +0530

    minor edit to equals method

--- src/model/ChessMen.java
+++ src/model/ChessMen.java
@@ -24,19 +24,19 @@ public class ChessMen
 	
 	public ChessMen(String name, int x, int y, String color)
 	{
-		this.name = name;
+		ChessMen(name, color);
 		this.position = new Coordinates(x, y);
-		this.setColor(color);
 	}
 	
-	public String toString()
-	{
+	public String toString() {
 		return getColor().charAt(0)+getName() ;
 	}
 	
 	@Override
 	public boolean equals(Object obj)
 	{
+		if(!(obj instanceof ChessMen))
+			return false;
 		ChessMen c = (ChessMen) obj;
 		return this.getName().trim().equalsIgnoreCase(c.getName().trim()) && this.getColor().trim().equalsIgnoreCase(c.getColor().trim());
 		
@@ -49,32 +49,28 @@ public class ChessMen
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getColor() 
-	{
+	public String getColor() {
 		return color;
 	}
-	public void setColor(String color) 
-	{
+	public void setColor(String color) {
 		this.color = color;
 	}
 
 	public Coordinates getPosition() {
 		return position;
 	}
-	public void setPosition(Coordinates position) 
-	{
+	public void setPosition(Coordinates position) {
 		this.position = position;
 	}
 	
 	@Override
-	public int hashCode()
-	{
+	public int hashCode() {
 		return (this.getName().trim() + this.getColor().trim()).hashCode();
 	}
 	
 	public ArrayList<Coordinates> getAllPossiblePositions(Data chessboard, Player player)
 	{
-		ArrayList<Coordinates> allPositions = new ArrayList<>();
+		ArrayList<Coordinates> allPositions = new ArrayList<Coordinates>();
 
 		for(int i=1; i<=8; i++)
 			for(int j=1; j<=8; j++)
